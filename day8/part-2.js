73bb1d7e4c9bf523197d0f860914129ae116d695
PlastikMaykr <129111623+PlastikMaykr@users.noreply.github.com>
2023-12-09 06:00:31 +0100

    day8 part2 - done
    Haunted Wasteland part2 done by determining steps cycle for each starting location and then finding the least common multiple (in trillions).

--- day8/part-2.js
+++ day8/part-2.js
@@ -22,26 +22,67 @@ for (let line of maps) {
 }
 // console.log(nodes);
 
-let steps = 0;
 let pins = Object.keys(nodes).filter(n => n.endsWith('A'));
-console.log(pins);
+let steps = [...Array(pins.length)].fill(0);
+console.log({ pins, steps });
 pins.every(n => n.endsWith('Z'));
-pins.endWithZ = function() {
-    return this.every(n => n.endsWith('Z'));
-}
-
-while (!pins.endWithZ()) {
+// pins.endWithZ = function() {
+//     return this.every(n => n.endsWith('Z'));
+// }
+let counter = 0;
+while (!pins.every(n => n.endsWith('Z'))) {
     // while (steps < 10) {
-    const index = steps % dirs.length;
+    const index = counter % dirs.length;
     const dir = dirs[index];
     // console.log(index);
-    
+
     for (let i = 0; i < pins.length; i++) {
         const pin = pins[i];
+        // console.log(pin);
+        if (pin.endsWith('Z')) continue;
         pins[i] = nodes[pin][dir];
+        steps[i]++;
     }
     console.log(pins);
 
-    steps++;
+    counter++;
+    // steps++;
 }
 console.log(steps);
+
+const stepsLCM = findLCM(steps);
+console.log(stepsLCM);
+
+function findLCM(nums) { // least common multiple
+    const divisors = [];
+    const numbers = [...nums];
+    let prime = 2;
+    const anyDivisibles = () => numbers.some(n => !(n % prime));
+
+    while (!numbers.every(n => n === 1)) {
+        while (!anyDivisibles()) {
+            prime = findNextPrime(prime);
+        }
+        divisors.push(prime);
+        for (let i = 0; i < numbers.length; i++) {
+            if (!(numbers[i] % prime)) numbers[i] /= prime;
+        }
+    }
+    return divisors.reduce((lcm, div) => lcm * div, 1);
+}
+
+function findNextPrime(num) {
+    var isPrime = false;
+    while (!isPrime) {
+        num++;
+        num % 2 || num++;
+        isPrime = true;
+        for (var i = 2; i < Math.ceil(num / 2); i++) {
+            if (!(num % i)) {
+                isPrime = false;
+                break;
+            }
+        }
+    }
+    return num;
+}
