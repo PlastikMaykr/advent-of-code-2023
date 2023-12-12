898ec9e6a4765ed19c1c89d96bcf2f4d9dc700ac
PlastikMaykr <129111623+PlastikMaykr@users.noreply.github.com>
2023-12-12 16:14:51 +0100

    day11 part1
    Cosmic Expansion part1 - done.

---
+++ day11/part-1.js
@@ -0,0 +1,69 @@
+const fs = require('node:fs');
+
+const data = fs.readFileSync('input.txt', 'utf8');
+const dataArr = data
+    .split('\r\n')
+    .map(d => d.split(''));
+// console.log(dataArr);
+
+// galaxies count
+let rows = [...Array(dataArr.length)].fill(0);
+let columns = [...Array(dataArr[0].length)].fill(0);
+for (let y = 0; y < dataArr.length; y++) {
+    for (let x = 0; x < dataArr[y].length; x++) {
+        const space = dataArr[y][x];
+        if (space === '#') {
+            // console.log(x, y);
+            rows[y]++;
+            columns[x]++;
+        }
+    }
+}
+console.log({ rows, columns });
+
+// expand
+const empty = '+';
+const expanded = [];
+for (let y = 0; y < dataArr.length; y++) {
+    const line = [];
+    expanded.push(line);
+    for (let x = 0; x < dataArr[y].length; x++) {
+        const space = dataArr[y][x];
+        line.push(space);
+        if (!columns[x]) line.push(empty);
+    }
+    if (!rows[y]) expanded.push([...line].fill(empty));
+}
+// console.log({ expanded });
+
+let output = [...expanded].join('\r\n').replaceAll(',', '');
+// console.log(output);
+fs.writeFileSync('output.txt', output);
+
+// galaxies coords
+const coords = []
+for (let y = 0; y < expanded.length; y++) {
+    for (let x = 0; x < expanded[y].length; x++) {
+        const space = expanded[y][x];
+        if (space === '#') {
+            // console.log(x,y);
+            coords.push([x, y]);
+        }
+    }
+}
+console.log({ coords });
+
+// galaxies distances
+const distances = []
+let distSum = 0;
+for (let i = 0; i < coords.length - 1; i++) {
+    for (let j = i + 1; j < coords.length; j++) {
+        const [x1, y1] = coords[i];
+        const [x2, y2] = coords[j];
+        const distance = Math.abs(x1 - x2) + Math.abs(y1 - y2);
+        distances.push(distance);
+        distSum += distance;
+        console.log({ i,j, distance});
+    }
+}
+console.log({ distances, distSum });
